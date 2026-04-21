17b757ff150decd4117c0e14065f3f9b5a32b9cd
jankrajci589-web <jan.krajci589@gmail.com>
2026-04-21 11:09:41 +0200

    Fix missing config constants

--- js/config.js
+++ js/config.js
@@ -3,6 +3,10 @@ export const CONFIG = Object.freeze({
   MAX_LEDGES: 30,
   HOUSE_EDGE: 0.04,
   LOSS_ZONE_MULTIPLIERS: Object.freeze([0.20, 0.25, 0.50, 0.75]),
+  LEDGE_WIDTH: 80,
+  LEDGE_HEIGHT: 16,
+  CANVAS_WIDTH: 480,
+  CANVAS_HEIGHT: 720,
 
   DIFFICULTIES: Object.freeze({
     easy:    { increment: 0.04 },
